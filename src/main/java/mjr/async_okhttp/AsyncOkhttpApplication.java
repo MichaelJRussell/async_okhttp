93a2e684eaaa646cce7f0e8b63c9603db367eeb0
Michael Russell <MichaelJRussell@users.noreply.github.com>
2022-11-24 17:30:55 -0600

    Initial commit

---
+++ src/main/java/mjr/async_okhttp/AsyncOkhttpApplication.java
@@ -0,0 +1,13 @@
+package mjr.async_okhttp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncOkhttpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncOkhttpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mjr/async_okhttp/AsyncOkhttpApplicationTests.java
@@ -0,0 +1,13 @@
+package mjr.async_okhttp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncOkhttpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
