39899976c0405a19c5e0640e8eaf51fff49d90ac
Michael Russell <MichaelJRussell@users.noreply.github.com>
2022-11-25 18:42:39 -0600

    Some code consolidation in HttpClient

--- src/main/java/mjr/async_okhttp/http/HttpClient.java
+++ src/main/java/mjr/async_okhttp/http/HttpClient.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.squareup.moshi.Moshi;
 import lombok.extern.slf4j.Slf4j;
-import mjr.async_okhttp.models.CreatePremisesRequest;
 import okhttp3.*;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
@@ -23,77 +22,32 @@ public class HttpClient
     private final OkHttpClient client = new OkHttpClient();
 
     public <T> CompletableFuture<T> get(String url, Map<String, String> headers, Class<T> responseType) {
-        var future = new CompletableFuture<T>();
         var requestBuilder = new Request.Builder()
             .url(BASE_URL + url);
 
         applyHeaders(requestBuilder, headers);
 
         var request = requestBuilder.build();
+        var call = client.newCall(request);
 
-        client.newCall(request).enqueue(new Callback() {
-             @Override
-             public void onFailure(@NotNull Call call, @NotNull IOException e) {
-                 future.completeExceptionally(e);
-             }
-
-             @Override
-             public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                 if (!response.isSuccessful()) {
-                     throw new IOException(String.format(
-                         "Unexpected code from url %s: %s",
-                         url,
-                         response));
-                 }
-
-                 var body = response.body();
-                 T responseObj = getResponseObject(body, responseType);
-
-                 future.complete(responseObj);
-             }
-         });
-
-        return future;
+        return performCall(call, responseType);
     }
 
     public <T> CompletableFuture<T> get(String url, Map<String, String> headers, Type responseType) {
-        var future = new CompletableFuture<T>();
         var requestBuilder = new Request.Builder()
             .url(BASE_URL + url);
 
         applyHeaders(requestBuilder, headers);
 
         var request = requestBuilder.build();
+        var call = client.newCall(request);
 
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(@NotNull Call call, @NotNull IOException e) {
-                future.completeExceptionally(e);
-            }
-
-            @Override
-            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                if (!response.isSuccessful()) {
-                    throw new IOException(String.format(
-                        "Unexpected code from url %s: %s",
-                        url,
-                        response));
-                }
-
-                var body = response.body();
-                T responseObj = getResponseObject(body, responseType);
-
-                future.complete(responseObj);
-            }
-        });
-
-        return future;
+        return performCall(call, responseType);
     }
 
     public <T> CompletableFuture<T> post(String url, Map<String, String> headers, Object body, Class<T> responseType)
         throws Exception
     {
-        var future = new CompletableFuture<T>();
         Request request;
 
         try {
@@ -112,7 +66,15 @@ public class HttpClient
             throw new Exception("Error building HTTP request", ex);
         }
 
-        client.newCall(request).enqueue(new Callback() {
+        var call = client.newCall(request);
+
+        return performCall(call, responseType);
+    }
+
+    private <T> CompletableFuture<T> performCall(Call call, Class<T> responseType) {
+        var future = new CompletableFuture<T>();
+
+        call.enqueue(new Callback() {
             @Override
             public void onFailure(@NotNull Call call, @NotNull IOException e) {
                 future.completeExceptionally(e);
@@ -123,7 +85,39 @@ public class HttpClient
                 if (!response.isSuccessful()) {
                     var ex = new IOException(String.format(
                         "Unexpected code from url %s: %s",
-                        url,
+                        call.request().url(),
+                        response));
+
+                    log.error(response.body().string());
+
+                    future.completeExceptionally(ex);
+                }
+
+                var body = response.body();
+                T responseObj = getResponseObject(body, responseType);
+
+                future.complete(responseObj);
+            }
+        });
+
+        return future;
+    }
+
+    private <T> CompletableFuture<T> performCall(Call call, Type responseType) {
+        var future = new CompletableFuture<T>();
+
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                future.completeExceptionally(e);
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    var ex = new IOException(String.format(
+                        "Unexpected code from url %s: %s",
+                        call.request().url(),
                         response));
 
                     log.error(response.body().string());
@@ -151,10 +145,8 @@ public class HttpClient
 
     private static <T> String json(T obj) throws JsonProcessingException
     {
-        //var adapter = MOSHI.adapter(CreatePremisesRequest.class);
         var objectMapper = new ObjectMapper();
 
-        //return adapter.toJson(obj);
         return objectMapper.writeValueAsString(obj);
     }
 
@@ -163,9 +155,6 @@ public class HttpClient
 
         if (responseType != null && body != null) {
             var bodyString = body.string();
-
-            log.info("Body: " + bodyString);
-
             var adapter = MOSHI.adapter(responseType);
 
             try {
